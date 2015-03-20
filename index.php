247bb66690e5c63f7da4d5688f3333b5ac150e8e
dpaschal <daniel.paschal@gmail.com>
2015-03-20 16:43:45 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Dan's Blog</title>
+    <script src="includes/js/jquery/jquery-2.1.3.min.js"></script>
+    <link rel="stylesheet" href="includes/bootstrap/bootstrap-3.3.4-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="includes/bootstrap/bootstrap-3.3.4-dist/css/bootstrap-theme.min.css">
+    <script src="includes/tinymce/js/tinymce/tinymce.min.js"></script>
+    <script src="includes/js/main.js"></script>
+    <link rel="stylesheet" href="includes/css/style.css">
+</head>
+<body>
+    <header id="main_header">
+        <nav id="main_nav" class="navbar navbar-default">
+            <ul id="main_menu" class="navbar-header">
+                <li><a>Home</a></li>
+                <li><a>Explore</a></li>
+                <li><a>Manage</a></li>
+                <li><a>Login</a></li>
+            </ul>
+            <nav id="user_info">
+                <img src="images/538474-user_512x512.png" class="avatar_small">
+                Hello, Daniel
+            </nav>
+        </nav>
+
+    </header>
+    <main id="main_content">
+    </main>
+
+
+
+</body>
+</html>
